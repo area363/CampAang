d978ba24cd77697e20b689103d291b57c00ad0d3
Ubuntu <ubuntu@ip-172-31-33-78.ap-northeast-2.compute.internal>
2020-07-02 22:15:57 +0000

    aws

--- src/BookingApp.jsx
+++ src/BookingApp.jsx
@@ -36,7 +36,7 @@ class BookingApp extends React.Component {
       console.log("Welcome!");
     } else {
       axios
-        .get(`http://localhost:3030/booking${id}`)
+        .get(`http://52.79.61.49:3030/booking${id}`)
         .then((data) => {
           var daylist = [];
           var result = [];
@@ -179,7 +179,7 @@ class BookingApp extends React.Component {
       console.log("Welcome!");
     } else {
       axios
-        .post(`http://localhost:3030/booking${id}`, booking)
+        .post(`http://52.79.61.49:3030/booking${id}`, booking)
         .then((res) => {
           console.log(res.data);
         })
